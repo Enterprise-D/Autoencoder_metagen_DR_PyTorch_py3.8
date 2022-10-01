e859161085e6218ce772b83d7286fda6ccfbee6d
Enterprise-J <sunjiachen-braxton@outlook.com>
2022-10-01 14:15:47 -0700

    Added prediction (temporally run on cpu)

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 # %%
 import random
-import torch as T
-import torch.nn as N
-import torch.optim as optim
 import numpy as np
 from Bio import SeqIO
+import torch as T
+import torch.nn as N
+import torch.optim as O
 import torch.nn.functional as F
 import torch.utils.data as D
 from torchsummary import summary
-
 
 # %%
 length = 128
@@ -43,23 +42,23 @@ class AE(N.Module):
 
     def __init__(self, **kwargs):
         super().__init__()
-        self.encoder_1 = N.Conv1d(in_channels=5, out_channels=128, kernel_size=5, stride=1,padding=2)
+        self.encoder_1 = N.Conv1d(in_channels=5, out_channels=128, kernel_size=5, stride=1, padding=2)
         self.encoder_2 = N.MaxPool1d(kernel_size=4, stride=4, return_indices=True)
-        self.encoder_3 = N.Conv1d(in_channels=128, out_channels=32, kernel_size=5, stride=1,padding=2)
+        self.encoder_3 = N.Conv1d(in_channels=128, out_channels=32, kernel_size=5, stride=1, padding=2)
         self.encoder_4 = N.MaxPool1d(kernel_size=4, stride=4, return_indices=True)
-        self.encoder_5 = N.Conv1d(in_channels=32, out_channels=8, kernel_size=5, stride=1,padding=2)
+        self.encoder_5 = N.Conv1d(in_channels=32, out_channels=8, kernel_size=5, stride=1, padding=2)
         self.encoder_6 = N.MaxPool1d(kernel_size=4, stride=4, return_indices=True)
-        self.encoder_7 = N.Conv1d(in_channels=8, out_channels=2, kernel_size=5, stride=1,padding=2)
+        self.encoder_7 = N.Conv1d(in_channels=8, out_channels=2, kernel_size=5, stride=1, padding=2)
 
-        # self.encoder_s = N.AvgPool1d(kernel_size=2, stride=2)
+        self.encoder_s = N.AvgPool1d(kernel_size=2, stride=2)
 
-        self.decoder_1 = N.ConvTranspose1d(in_channels=2, out_channels=8, kernel_size=5, stride=1,padding=2)
+        self.decoder_1 = N.ConvTranspose1d(in_channels=2, out_channels=8, kernel_size=5, stride=1, padding=2)
         self.decoder_2 = N.MaxUnpool1d(kernel_size=4, stride=4)
-        self.decoder_3 = N.ConvTranspose1d(in_channels=8, out_channels=32, kernel_size=5, stride=1,padding=2)
+        self.decoder_3 = N.ConvTranspose1d(in_channels=8, out_channels=32, kernel_size=5, stride=1, padding=2)
         self.decoder_4 = N.MaxUnpool1d(kernel_size=4, stride=4)
-        self.decoder_5 = N.ConvTranspose1d(in_channels=32, out_channels=128, kernel_size=5, stride=1,padding=2)
+        self.decoder_5 = N.ConvTranspose1d(in_channels=32, out_channels=128, kernel_size=5, stride=1, padding=2)
         self.decoder_6 = N.MaxUnpool1d(kernel_size=4, stride=4)
-        self.decoder_7 = N.ConvTranspose1d(in_channels=128, out_channels=5, kernel_size=5, stride=1,padding=2)
+        self.decoder_7 = N.ConvTranspose1d(in_channels=128, out_channels=5, kernel_size=5, stride=1, padding=2)
 
     def forward(self, x0):
         x1 = T.relu(self.encoder_1(x0))
@@ -68,9 +67,9 @@ class AE(N.Module):
         x4, i4 = self.encoder_4(x3)
         x5 = T.relu(self.encoder_5(x4))
         x6, i6 = self.encoder_6(x5)
-        x7 = T.relu(self.encoder_7(x6))
+        x7 = T.tanh(self.encoder_7(x6))
 
-        # encoded = T.relu(self.encoder_8(x7))
+        encoded = self.encoder_s(x7)
 
         x8 = T.relu(self.decoder_1(x7))
         x9 = self.decoder_2(x8, i6)
@@ -78,15 +77,15 @@ class AE(N.Module):
         x11 = self.decoder_4(x10, i4)
         x12 = T.relu(self.decoder_5(x11))
         x13 = self.decoder_6(x12, i2)
-        x14 = T.relu(self.decoder_7(x13))
+        x14 = T.sigmoid(self.decoder_7(x13))
 
-        return x14
+        return x14, encoded
 
 
 # %%
 #  use gpu if available
 device = T.device("mps" if T.has_mps else "cpu")
-#device = 'cpu'
+device = 'cpu'
 
 # create a model from `AE` autoencoder class
 # load it to the specified device, either gpu or cpu
@@ -95,13 +94,14 @@ summary(AE(input_shape=(length, 5)).to('cpu'), input_size=(5, 128))
 
 # create an optimizer object
 # Adam optimizer with learning rate 1e-3
-optimizer = optim.Adam(full_model.parameters(), lr=1e-3)
+optimizer = O.Adam(full_model.parameters(), lr=1e-3)
 
 # mean-squared error loss
 criterion = N.MSELoss()
 
 # %%
 sequence_tensor = T.from_numpy(sequence_matrix).to(device)
+
 
 class SequenceDataset(D.Dataset):
     def __init__(self):
@@ -119,13 +119,13 @@ class SequenceDataset(D.Dataset):
 # %%
 train_dataloader = D.DataLoader(SequenceDataset(), batch_size=64, shuffle=True)
 
-epochs = 100
+epochs = 10
 for epoch in range(epochs):
     loss = 0
     for inputs in train_dataloader:
         optimizer.zero_grad()
 
-        outputs = full_model(inputs)
+        outputs, _ = full_model(inputs)
 
         train_loss = criterion(outputs, inputs)
 
@@ -141,3 +141,10 @@ for epoch in range(epochs):
 
     # display the epoch training loss
     print("epoch : {}/{}, loss = {:.6f}".format(epoch + 1, epochs, loss))
+
+latent = []
+
+with T.no_grad():
+    for inputs in train_dataloader:
+        _, outputs = full_model(inputs)
+        latent.append(outputs)
